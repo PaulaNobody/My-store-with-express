1ba320514b8e40ebd84f2df533d6dc1b6576acb6
PaulaNobody <1070331217cc@gmail.com>
2022-06-22 08:22:37 -0500

    primer commit con heroku

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const cors= require('cors');
+const cors  = require('cors');
 const routerApi= require('./routes');
 
 const { logErrors, errorHandler, boomerrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port=process.env.PORT||3000;
 
 app.use(express.json());
 
-const whitelist=['http://localhost:8080','https://myapp.co'];
-const options={
-  origin: (origin, callback)=>{
-    if(whitelist.includes(origin)){
-      callback(null,true);
-    }else{
-      callback(new Error('no permitido'));
-    }
-  }
-}
-app.use(cors(options));// habilita a cualquier dominio
-
+// const whitelist=['http://localhost:8080'];
+// const options={
+//   origin: (origin, callback)=>{
+//     if(whitelist.includes(origin)){
+//       callback(null,true);
+//     }else{
+//       callback(new Error('no permitido'));
+//     }
+//   }
+// }
+// app.use(cors(options));// habilita a cualquier dominio
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
--- routes/categories.router.js
+++ routes/categories.router.js
@@ -27,8 +27,8 @@ router.get('/:id',
   validatorHandler( createCategorySchema, 'body'),
   async (req, res) => {
     const body = req.body;
-    const newPCategory = await service.create(body);
-    res.status(201).json(newPCategory);
+    const newCategory = await service.create(body);
+    res.status(201).json(newCategory);
   })
 
   router.patch('/:id',
